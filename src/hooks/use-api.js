2adf5d75f69b3625565c4b1d13eaaad9b1962fbb
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-23 02:55:25 +0300

    modified the custom hook

--- src/hooks/use-api.js
+++ src/hooks/use-api.js
@@ -1,10 +1,25 @@
 import react, { useState } from "react";
 import axios from "axios";
-const useApi = (requestConfig) => {
-  const [data, setSata] = useState();
+const useApi = (requestConfig, applyData) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
   const getData = async () => {
-    await axios.get();
+    setIsLoading(true);
+    try {
+      const res = await axios.get(`${requestConfig.url}`).then((response) => {
+        setData(response.data);
+        applyData(response.data);
+        console.log("response data", response);
+
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setError(error);
+      console.log("error when fetching data");
+    }
   };
+  return { data, error, isLoading, getData };
 };
 
 export default useApi;

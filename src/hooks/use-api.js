2e8fa4d8b98fb9df33c029d0466550a5b13c2f24
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-23 02:20:16 +0300

    created a custom api hook

---
+++ src/hooks/use-api.js
@@ -0,0 +1,10 @@
+import react, { useState } from "react";
+import axios from "axios";
+const useApi = (requestConfig) => {
+  const [data, setSata] = useState();
+  const getData = async () => {
+    await axios.get();
+  };
+};
+
+export default useApi;

aa14ef0cd90d2ef3d6518485edc8fb956aa32784
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-23 05:49:59 +0300

    initialised the custom form hook

--- src/hooks/use-form.js
+++ src/hooks/use-form.js
@@ -1,1 +1,18 @@
-c;
+import React, { useState } from "react";
+const useForm = () => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const enteredValueHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+  };
+  const resetInput = () => {
+    setEnteredValue("");
+  };
+};
+export default useForm;
